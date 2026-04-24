fdf151893403d682db6ab10df5dffce6ec91a800
Ethan Rubenstein <ethanmrubenstein@gmail.com>
2026-04-24 10:30:54 -0400

    Enhances URL schema validation and structure

--- models/Url.js
+++ models/Url.js
@@ -3,29 +3,30 @@ const { nanoid } = require("nanoid");
 
 const ID_LENGTH = process.env.ID_LENGTH || 10;
 
-const UrlSchema = new mongoose.Schema({
-  full: {
-    type: String,
-    match: /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/,
-    required: true,
+const UrlSchema = new mongoose.Schema(
+  {
+    full: {
+      type: String,
+      match:
+        /^(https?:\/\/)?([\da-zA-Z.-]+)\.([a-zA-Z.]{2,6})(:\d+)?(\/[^\s]*)?$/,
+      required: true,
+    },
+    slug: {
+      type: String,
+      default: () => nanoid(ID_LENGTH),
+      unique: true,
+      maxLength: 10,
+    },
+    clicks: {
+      type: Number,
+      default: 0,
+    },
+    lastClickedAt: {
+      type: Date,
+      default: null,
+    },
   },
-  slug: {
-    type: String,
-    default: nanoid(ID_LENGTH),
-    maxLength: 10,
-  },
-  clicks: {
-    type: Number,
-    default: 0,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  lastClickedAt: {
-    type: Date,
-    default: null,
-  },
-});
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("Url", UrlSchema);
