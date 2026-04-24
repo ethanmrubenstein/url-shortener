6b347ef5e10382d91891488531dff75fd3b64166
Ethan Rubenstein <ethanmrubenstein@gmail.com>
2026-04-24 10:30:54 -0400

    Refactors database connection setup

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
-const connectDB = () => {
+const connectDB = async () => {
   mongoose.connect(process.env.MONGO_CONN_STRING);
+  console.log("MongoDB Connected Successfully!");
 };
 
 module.exports = connectDB;
